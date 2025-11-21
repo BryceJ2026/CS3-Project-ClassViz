978175f9503655aeb5d9032ae87cb4570253da81
Bryce? <bryce.jones2026@gbwl.org>
2025-11-21 14:39:51 +0000

    Made a WISE pie.

--- main.py
+++ main.py
@@ -7,11 +7,17 @@ plt.style.use('seaborn-v0_8-pastel')
 
 # Read CSV into DataFrame
 df = pd.read_csv('classdatav1.csv')
+df = df.head(8)
 print(df.info())
 colors = ['yellow', 'green']
 
 # NOTE: some columns need data type converted (object -> boolean)
-plt.pie(df['Wisdom Teeth Removed'].dropna(), startangle=270)
 
-plt.title('Wise vs Wisdom Teeth Removed')
-plt.savefig('wisebar.png', bbox_inches='tight')
+df['Wise'] = pd.to_numeric(df['Wise']).fillna(0).astype(bool)
+df['Wisdom Teeth Removed'] = pd.to_numeric(df['Wisdom Teeth Removed']).fillna(0).astype(bool)
+teethe = df['Wisdom Teeth Removed'].value_counts()
+plt.pie(teethe, colors=colors, labels=teethe, startangle=20)
+plt.axis('equal')
+
+plt.title('Wisdom Teeth Removed')
+plt.savefig('wisepie.png', bbox_inches='tight')
